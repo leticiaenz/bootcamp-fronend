bb9af97b8cbc727c0c7f5f1cd6e7571a42ce8476
leticiaenz <leticia.enz11@gmail.com>
2020-04-12 17:35:15 -0400

    Class 08: Listing api projects

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 import "./App.css";
-import backgroundImage from "./assets/background.jpeg";
 
 import Header from "./components/Header";
 
 function App() {
-  const [projects, setProjects] = useState([
-    "Desenvolvimento de app",
-    "Front-end web",
-  ]);
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get("projects").then((response) => {
+      setProjects(response.data);
+    });
+  }, []);
 
   function handleAddProject() {
-    // projects.push(`Novo Projeto ${Date.now()}`);
-    // console.log(projects);
-
     setProjects([...projects, `Novo Projeto ${Date.now()}`]);
   }
   return (
     <>
       <Header title="Projects" />
-      <img width={150} src={backgroundImage} alt="" />
 
       <ul>
         {projects.map((project) => (
-          <li key={project}>{project}</li>
+          <li key={project.id}>{project.title}</li>
         ))}
       </ul>
 
