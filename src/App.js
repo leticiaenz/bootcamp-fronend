1627766594a42080b898c1a520d99d0254ce37ea
leticiaenz <leticia.enz11@gmail.com>
2020-04-11 20:35:46 -0400

    Class 06: state and immutability

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 
 function App() {
+  const [projects, setProjects] = useState([
+    "Desenvolvimento de app",
+    "Front-end web",
+  ]);
+
+  function handleAddProject() {
+    // projects.push(`Novo Projeto ${Date.now()}`);
+    // console.log(projects);
+
+    setProjects([...projects, `Novo Projeto ${Date.now()}`]);
+  }
   return (
     <>
-      <Header title="Homepage">
-        <ul>
-          <li>HomePage</li>
-          <li>Projects</li>
-        </ul>
-      </Header>
-      <Header title="Projects">
-        <ul>
-          <li>HomePage</li>
-          <li>Projects</li>
-        </ul>
-      </Header>
+      <Header title="Projects" />
+      <ul>
+        {projects.map((project) => (
+          <li key={project}>{project}</li>
+        ))}
+      </ul>
+
+      <button type="button" onClick={handleAddProject}>
+        Adicionar Projeto
+      </button>
     </>
   );
 }
